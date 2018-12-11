0fa085d287cfa841d277189e52b5cbf80a103f21
Angélica Guamán Albarracín <angelica.guaman.albarracin@gmail.com>
2018-12-11 20:57:24 +0100

    se añade la opcion --json al script

--- src/Script/create_one_user.php
+++ src/Script/create_one_user.php
@@ -14,7 +14,7 @@ $dotenv->load();
 
 $entityManager = Utils::getEntityManager();
 
-if ($argc !== 4) {
+if ($argc < 4 || $argc > 5) {
     $fich = basename(__FILE__);
     echo <<< MARCA_FIN
 
@@ -38,7 +38,11 @@ $user->setIsAdmin(false);
 try {
     $entityManager->persist($user);
     $entityManager->flush();
-    echo 'Se ha creado el usuario ' . $user->getUsername() . 'con ID ' . $user->getId() . PHP_EOL;
+    if (in_array('--json', $argv, true)) {
+        echo json_encode($user, JSON_PRETTY_PRINT) . PHP_EOL;
+    }
+    echo 'Se ha creado el usuario ' . $user->getUsername() . ' con ID ' . $user->getId() . PHP_EOL;
+
 } catch (Exception $exception) {
     echo $exception->getMessage() . PHP_EOL;
 }
