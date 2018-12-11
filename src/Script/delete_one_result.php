d7f39216e6e97dd3656a1c8228af2da65ed3e47e
Angélica Guamán Albarracín <angelica.guaman.albarracin@gmail.com>
2018-12-11 21:06:29 +0100

    se añade la opcion --json al script

--- src/Script/delete_one_result.php
+++ src/Script/delete_one_result.php
@@ -14,7 +14,7 @@ $dotenv->load();
 
 $entityManager = Utils::getEntityManager();
 
-if ($argc !== 2) {
+if ($argc < 2 || $argc > 3) {
     $fich = basename(__FILE__);
     echo <<< MARCA_FIN
 
@@ -35,5 +35,10 @@ if ($result === null) {
 } else {
     $entityManager->remove($result);
     $entityManager->flush();
+
+    if (in_array('--json', $argv, true)) {
+        echo json_encode($result, JSON_PRETTY_PRINT) . PHP_EOL;
+    }
+
     echo 'El resultado con ID ' . $resultadoId . ' se ha eliminado' . PHP_EOL;
 }
